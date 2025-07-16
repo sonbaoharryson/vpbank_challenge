e9826e7067ef7fd4d4897cdf9c296ac88d9543bd
HarrySon <104372010+sonbaoharryson@users.noreply.github.com>
2025-07-16 10:56:03 +0700

    Update memory.py

--- storage/memory.py
+++ storage/memory.py
@@ -5,9 +5,9 @@ MAX_GENERATED = 100
 GENERATED_TRANSACTIONS: List[Transaction] = []
 
 def add_transaction(txn: Transaction):
+    if len(GENERATED_TRANSACTIONS) >= MAX_GENERATED:
+        GENERATED_TRANSACTIONS = []
     GENERATED_TRANSACTIONS.append(txn)
-    if len(GENERATED_TRANSACTIONS) > MAX_GENERATED:
-        GENERATED_TRANSACTIONS.pop(0)
 
 def get_transactions() -> List[Transaction]:
     return GENERATED_TRANSACTIONS
